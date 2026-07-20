3cdc8d29351c2e1610f4c248ef204c316f806c83
Rubellite <67942706+Trdsphr@users.noreply.github.com>
2026-07-20 12:36:56 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+
+title:
+"SignCraft 3D | Custom Handmade House Signs",
+
+description:
+"Create personalised handmade house signs."
+
+};
+
+
+export default function RootLayout({
+
+children,
+
+}: {
+
+children: React.ReactNode
+
+}) {
+
+
+return (
+
+<html lang="en">
+
+<body>
+
+{children}
+
+</body>
+
+</html>
+
+)
+
+}
